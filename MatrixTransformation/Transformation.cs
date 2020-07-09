836cce68811048f90d15a0acea2090daeb55e65b
zzhaozhuo <1484162391@qq.com>
2020-07-10 00:14:24 +0800

    remote commit

---
+++ MatrixTransformation/Transformation.cs
@@ -0,0 +1,15 @@
+﻿/**************************************************
+writer：zzhaozhuo
+time：#CreateTime#
+name：Transformation
+function：转换基类
+**************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Transformation : MonoBehaviour
+{
+    public abstract Vector3 Apply(Vector3 point);
+}
---
+++ MatrixTransformation/TransformationGrid.cs
@@ -0,0 +1,24 @@
+﻿/**************************************************
+writer：
+time：#CreateTime#
+name：
+function：
+**************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformationGrid : MonoBehaviour
+{
+    List<Transformation> transformations;
+    // Start is called before the first frame update
+    void Awake() {
+        transformations = new List<Transformation>();
+    }
+    void Update()
+    {
+        //GetComponent<Transformation>(transformations);
+        
+    }
+}
